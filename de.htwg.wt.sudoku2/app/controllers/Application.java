ce3abcc299d8344aee83d5ed441393e018d9d36f
markoboger <marko.boger@gmail.com>
2015-10-27 23:44:29 +0100

    got styles to work with less

--- de.htwg.wt.sudoku2/app/controllers/Application.java
+++ de.htwg.wt.sudoku2/app/controllers/Application.java
@@ -2,12 +2,16 @@ package controllers;
 
 import de.htwg.sudoku.Sudoku;
 import de.htwg.sudoku.aview.tui.TextUI;
+import de.htwg.sudoku.controller.ISudokuController;
 import play.*;
 import play.mvc.*;
 
 import views.html.*;
 
 public class Application extends Controller {
+	
+	static ISudokuController controller = Sudoku.getInstance().getController();
+
 
     public Result index() {
     	return ok(index.render("Welcome to Sudoku"));
@@ -16,7 +20,7 @@ public class Application extends Controller {
     public Result sudoku(String command) {
     	TextUI tui=Sudoku.getInstance().getTui();
     	tui.processInputLine(command);   	
-        return ok(sudoku.render(tui.toHtml()));
+        return ok(sudoku.render(controller));
     }
 
 }
