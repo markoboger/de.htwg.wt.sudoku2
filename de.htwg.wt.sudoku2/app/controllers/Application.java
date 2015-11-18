0d28c8efb8de0675bc5a8ed3c6f49585fc52529b
marko.boger@gmail.com <marko.boger@gmail.com>
2015-11-18 12:52:01 +0100

    started work on JavaScript, game works without refresh

--- de.htwg.wt.sudoku2/app/controllers/Application.java
+++ de.htwg.wt.sudoku2/app/controllers/Application.java
@@ -3,7 +3,7 @@ package controllers;
 import de.htwg.sudoku.Sudoku;
 import de.htwg.sudoku.aview.tui.TextUI;
 import de.htwg.sudoku.controller.ISudokuController;
-import play.*;
+import play.libs.Json;
 import play.mvc.*;
 
 import views.html.*;
@@ -22,5 +22,19 @@ public class Application extends Controller {
     	tui.processInputLine(command);   	
         return ok(sudoku.render(controller));
     }
+    
+    public Result showCandidates(int row, int column) {
+    	controller.showCandidates(row, column);
+    	String data = "Server shows Candidate";
+    	return ok(Json.toJson(data));
+    }
+    
+    public Result setValue(int row, int column, int value) {
+    	controller.setValue(row, column, value);
+    	String grid = controller.getGridString();
+    	return ok(Json.toJson(grid));
+    }
+    
+    
 
 }
