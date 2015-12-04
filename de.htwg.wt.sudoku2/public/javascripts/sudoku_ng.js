0795c3835fc399edaccba427aa40c7edfd24b421
marko.boger@gmail.com <marko.boger@gmail.com>
2015-12-04 13:14:41 +0100

    tried to add html, but it does not work, close though

--- de.htwg.wt.sudoku2/public/javascripts/sudoku_ng.js
+++ de.htwg.wt.sudoku2/public/javascripts/sudoku_ng.js
@@ -1,45 +1,40 @@
-$(function() {
+ var sudokuApp = angular.module('sudokuApp', []);
+ sudokuApp.controller('SudokuCtrl', function ($scope, $http){
+ 	$http.get('/json/').success(function(data) {  
+ 	   $scope.size = data.size;
+       $scope.grid = data.grid;  
+       $scope.cellclicked = function( row, column, value) {
+    	   if (value == "\u00A0") {
+           	   $http.get('/json/'+row+'/'+column).success(function(data) {
+           		   $scope.size = data.size;
+                   $scope.grid = data.grid; 
+                   fillGrid($scope.grid);
+           	   });
+    	   }
+       }
+       $scope.candidates=[]
+       fillGrid($scope.grid, $scope.candidates);
+     });   
+ 	
+ 	
+ });
 
-	$(".cell").click(function(event){
-		var row = $(this).attr("data-row");
-		var column = $(this).attr("data-column");
-		console.log("Received click at cell ("+ row + "," + column+")");
-		setShowCandidates(row, column);
-	});
-	$(".candidatecell").click(function(event) {
-		var row = $(this).attr("data-row");
-		var column = $(this).attr("data-column");
-		var value = $(this).attr("data-value");
-		console.log("Received click at candidate ("+ row + "," + column + "," + value + ")");
-		setValue(row, column, value);
-	});
-});
 
-
-function setValue(row,column,value) {
-	console.log("Received click at cell ("+ row + "," + column+")");
-
-    $.get("/json/" + row +"/"+ column +"/"+ value, function (data) {
-        fillGrid(data);
-    });
-}
-
-function setValue() {
-	console.log("function called");
-}
-
-function setShowCandidates(row,column) {
-    $.get("/json/" + row +"/"+ column, function (data) {
-        fillGrid(data);
-    });
-}
-
-function fillGrid(grid) {
+function fillGrid(grid, candidates) {
     var arrayLength = grid.length;
     for (var i = 0; i < arrayLength; i++) {
         var cell = grid[i];
         var id=cell.row*10+cell.column;
         if (cell.value == 0) {
+        	if (cell.candidates) {
+        		var candidateCell = angular.element(document.querySelector('#cell'+id))
+        		console.log("found a cell with candidates at "+id + " with candidates " + candidates);
+    		    candidates.splice(id,0,'' +
+					'<div class="candidatecell" ng-repeat="candidate in cell.candidates" >'+
+						'<p class="candidatetext">9</p>' +
+					'</div>' 
+				);
+        	}
         	cell.value ="\u00A0";
         }
     }
