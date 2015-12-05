0b96647b55438e9186e7dc7ea64b510e64c2700f
markoboger <marko.boger@gmail.com>
2015-12-05 23:14:32 +0100

    got directives in Angular to work, css is fixed too

--- de.htwg.wt.sudoku2/public/javascripts/sudoku_ng.js
+++ de.htwg.wt.sudoku2/public/javascripts/sudoku_ng.js
@@ -1,11 +1,34 @@
  var sudokuApp = angular.module('sudokuApp', ['ngSanitize']);
  
- angular.module('sudokuApp')
- .filter('to_trusted', ['$sce', function($sce){
+ sudokuApp.filter('to_trusted', ['$sce', function($sce){
      return function(text) {
          return $sce.asTrustedHtml(text);
      };
  }]);
+ 
+ sudokuApp.directive('sudokuGrid' , function () {
+		return {
+			templateUrl: 'html/sudokuGrid.html'
+		}; 
+	 });
+	 
+ sudokuApp.directive('sudokuCell' , function () {
+	return {
+		templateUrl: 'html/sudokuCell.html'
+	}; 
+ });
+ 
+ sudokuApp.directive('sudokuValuecell' , function () {
+		return {
+			templateUrl: 'html/sudokuValueCell.html'
+		}; 
+	 });
+ 
+ sudokuApp.directive('sudokuCandidatecell' , function () {
+		return {
+			templateUrl: 'html/sudokuCandidateCell.html'
+		}; 
+	 });
  
  sudokuApp.controller('SudokuCtrl', function ($scope, $http){
  	$http.get('/json/').success(function(data) {  
