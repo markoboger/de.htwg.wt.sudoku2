fa3c379c5a59c7bb2ca561e786d0d83acd93d7bf
markoboger <marko.boger@gmail.com>
2015-12-05 20:06:22 +0100

    runs with click on cell but not with click on candidate, css broken

--- de.htwg.wt.sudoku2/public/javascripts/sudoku_ng.js
+++ de.htwg.wt.sudoku2/public/javascripts/sudoku_ng.js
@@ -1,42 +1,61 @@
- var sudokuApp = angular.module('sudokuApp', []);
+ var sudokuApp = angular.module('sudokuApp', ['ngSanitize']);
+ 
+ angular.module('sudokuApp')
+ .filter('to_trusted', ['$sce', function($sce){
+     return function(text) {
+         return $sce.asTrustedHtml(text);
+     };
+ }]);
+ 
  sudokuApp.controller('SudokuCtrl', function ($scope, $http){
  	$http.get('/json/').success(function(data) {  
  	   $scope.size = data.size;
        $scope.grid = data.grid;  
+       $scope.cellHtml=[]
        $scope.cellclicked = function( row, column, value) {
+    	   console.log("cell clicked" );
     	   if (value == "\u00A0") {
            	   $http.get('/json/'+row+'/'+column).success(function(data) {
            		   $scope.size = data.size;
                    $scope.grid = data.grid; 
-                   fillGrid($scope.grid);
+                   fillGrid($scope.size, $scope.grid, $scope.cellHtml);
            	   });
     	   }
        }
-       $scope.candidates=[]
-       fillGrid($scope.grid, $scope.candidates);
+       $scope.candidateclicked = function( row, column, value) {
+    	   console.log("candidate clicked" );
+       	   $http.get('/json/'+row+'/'+column+'/'+value).success(function(data) {
+       		   $scope.size = data.size;
+               $scope.grid = data.grid; 
+               fillGrid($scope.size, $scope.grid, $scope.cellHtml);
+       	   });
+       }
+    
+       fillGrid($scope.size, $scope.grid, $scope.cellHtml);
      });   
- 	
- 	
  });
 
 
-function fillGrid(grid, candidates) {
+function fillGrid(size, grid, cellHtml) {
     var arrayLength = grid.length;
     for (var i = 0; i < arrayLength; i++) {
         var cell = grid[i];
         var id=cell.row*10+cell.column;
         if (cell.value == 0) {
-        	if (cell.candidates) {
-        		var candidateCell = angular.element(document.querySelector('#cell'+id))
-        		console.log("found a cell with candidates at "+id + " with candidates " + candidates);
-    		    candidates.splice(id,0,'' +
-					'<div class="candidatecell" ng-repeat="candidate in cell.candidates" >'+
-						'<p class="candidatetext">9</p>' +
-					'</div>' 
-				);
-        	}
-        	cell.value ="\u00A0";
-        }
+        	var candidateCell = angular.element(document.querySelector('#cell'+id))
+        	if (cell.candidates !== undefined) {
+        		var html="";
+        		for (var j = 0; j < size; j++ )	{	
+        			if (cell.candidates[j]) {
+			    		html = html + '<div class="candidate" ng-click="candidateclicked(cell.row,cell.column,'+ (j+1) +') ">' + (j+1) + '</div>' ;
+        			} else {
+        				html = html + '<div class="candidate"> &nbsp; </div>';
+        			};
+        		};
+        		cellHtml.splice(id,0,html);
+        	} else
+        	cellHtml.splice(id,0,"\u00A0");
+        } else cellHtml.splice(id,0,cell.value); 	
     }
 }
 
